786bdafdb8ef2e276681a8549a95c4644064ee9a
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-28 01:10:03 +0100

    Add project files.

---
+++ Kata_Bowling/src/BowlingGame.cs
@@ -0,0 +1,74 @@
+﻿namespace Kata_Bowling;
+
+public class BowlingGame
+{
+    private int _currentRoll;
+    private readonly int[] _rolls = new int[21];
+
+    public void Roll(int i)
+    {
+        _rolls[_currentRoll] = i;
+
+        var isLastPossibleRoll = _currentRoll == 20;
+        var isStrike = i == 10;
+
+        _currentRoll += isLastPossibleRoll ? 0 : isStrike ? 2 : 1;
+    }
+
+    public int Score()
+    {
+        var output = 0;
+
+        for (var currentRoll = 0; currentRoll < _rolls.Length; currentRoll += 2)
+        {
+            output += _rolls[currentRoll];
+            var isLastPossibleRoll = currentRoll == 20;
+
+            if (isLastPossibleRoll)
+                continue;
+
+            output += _rolls[currentRoll + 1];
+            output = ApplyBonus(currentRoll, output);
+        }
+
+        return output;
+    }
+
+    private int ApplyBonus(int currentRoll, int output)
+    {
+        output += ApplySpareBonus(currentRoll);
+        output += ApplyStrikeBonus(currentRoll);
+
+        return output;
+    }
+
+    private int ApplySpareBonus(int currentRoll)
+    {
+        var bonus = 0;
+
+        var isStrike = _rolls[currentRoll] == 10;
+        var isSpare = !isStrike && _rolls[currentRoll] + _rolls[currentRoll + 1] == 10;
+
+        if (isSpare)
+            bonus = _rolls[currentRoll + 2];
+
+        return bonus;
+    }
+
+    private int ApplyStrikeBonus(int currentRoll)
+    {
+        var isStrike = _rolls[currentRoll] == 10;
+
+        if (!isStrike)
+            return 0;
+
+        var isTwoStrikesInRow = isStrike && _rolls[currentRoll + 2] == 10;
+        var isPenultimateFrame = currentRoll == 18;
+
+        var bonusFirstBall = _rolls[currentRoll + 2];
+        var bonusSecondBall =
+            isPenultimateFrame ? 0 : (!isTwoStrikesInRow ? _rolls[currentRoll + 3] : _rolls[currentRoll + 4]);
+
+        return bonusFirstBall + bonusSecondBall;
+    }
+}
---
+++ Kata_Bowling/test/BowlingGameTest.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Kata_Bowling.Test;
+    
+public class BowlingGameTest
+{
+    protected BowlingGame Game { get; set; } = new();
+
+    protected void RollMany(int n, int pins)
+    {
+        for (var i = 0; i < n; i++)
+            Game.Roll(pins);
+    }
+
+    [Fact]
+    public void TestGutterGame()
+    {
+        RollMany(20, 0);
+        Game.Score().Should().Be(0);
+    }
+
+    [Fact]
+    public void TestAllOnes()
+    {
+        RollMany(20, 1);
+        Game.Score().Should().Be(20);
+    }
+
+    [Fact]
+    public void TestOneSpare()
+    {
+        Game.Roll(5);
+        Game.Roll(5);
+        Game.Roll(3);
+        RollMany(17, 0);
+        Game.Score().Should().Be(16);
+    }
+
+    [Fact]
+    public void TestOneStrike()
+    {
+        Game.Roll(10);
+        Game.Roll(3);
+        Game.Roll(4);
+        RollMany(16, 0);
+        Game.Score().Should().Be(24);
+    }
+
+    [Fact]
+    public void TestPerfectGame()
+    {
+        RollMany(12, 10);
+        Game.Score().Should().Be(300);
+    }
+}
